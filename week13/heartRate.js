833d6c4a23adea428593191064caddca68ae74d4
amberleedavis <may16006@byui.edu>
2020-07-18 18:31:16 -0600

    heartrate

---
+++ week13/heartRate.js
@@ -0,0 +1,42 @@
+    "use strict";
+
+    /* Defining Table
+     * Input: Ask user for age
+     * Processing: max = (220-age);
+     *   Slowest max * 65%
+     *   Fastest max * 85%
+     * Output: slowest and fastest beneficial heart rates
+     */
+
+    function beneficialHeartRates() {
+        // Ask user for age
+        var age = parseInt(document.getElementById('age').value);
+
+        // Compute the slowest and fastest benefical heart rates
+        var max = 220 - age;
+        var slowest = max * 0.65;
+        var fastest = max * 0.85;
+
+
+        // Display the beneficial rates for user to see
+        document.getElementById('outputDiv1').innerHTML = slowest;
+        document.getElementById('outputDiv2').innerHTML = fastest;
+
+    }
+
+    function calculateHeartRate() {
+        // Ask user for heart beat
+        var beat = parseInt(document.getElementById('beat').value);
+
+        // Compute the heart beat
+
+        var calculate = beat * 6;
+
+        // display the heart rate
+        document.getElementById('outputDiv3').innerHTML = calculate;
+    }
+
+// Store
+localStorage.setItem("calculate", "beat");
+// Retrieve
+document.getElementById("result").innerHTML = localStorage.getItem("calculate");
